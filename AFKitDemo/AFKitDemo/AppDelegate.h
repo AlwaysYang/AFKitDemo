2bbeea65309b0f7c3038dff0740bea2f3a97d9ad
AlwaysYang <425611381@qq.com>
2016-11-08 22:46:54 +0800

    new project

---
+++ AFKitDemo/AFKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFKitDemo
+//
+//  Created by AlwaysYang on 16/11/8.
+//  Copyright © 2016年 AlwaysYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFKitDemo/AFKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFKitDemo
+//
+//  Created by AlwaysYang on 16/11/8.
+//  Copyright © 2016年 AlwaysYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
