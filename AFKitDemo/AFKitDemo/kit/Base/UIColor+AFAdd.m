3ac53f2344884daf141a20c9bf42a1ad320194fe
无言是非丶 <zhuweishi@zhuweishideiMac.local>
2016-11-09 17:48:17 +0800

    add UIkit category

---
+++ AFKitDemo/AFKitDemo/kit/Base/UIColor+AFAdd.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+AFAdd.m
+//  AFKitDemo
+//
+//  Created by 猪卫士 on 16/11/9.
+//  Copyright © 2016年 AF. All rights reserved.
+//
+
+#import "UIColor+AFAdd.h"
+
+@implementation UIColor (AFAdd)
+
+@end
---
+++ AFKitDemo/AFKitDemo/kit/Base/UIView+AFAdd.m
@@ -0,0 +1,13 @@
+//
+//  UIView+AFAdd.m
+//  AFKitDemo
+//
+//  Created by 猪卫士 on 16/11/9.
+//  Copyright © 2016年 AF. All rights reserved.
+//
+
+#import "UIView+AFAdd.h"
+
+@implementation UIView (AFAdd)
+
+@end
